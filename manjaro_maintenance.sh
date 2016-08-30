25cd9c8803a1eb20dfa3421583c8497dc3a62312
raindog2 <raindog2@users.noreply.github.com>
2016-08-30 15:33:41 -0700

    Update manjaro_maintenance.sh

--- manjaro_maintenance.sh
+++ manjaro_maintenance.sh
@@ -12,6 +12,6 @@ yaourt -Sc --noconfirm
 echo -e "\n\033[1;32mNow finding and deleting orphaned packages (yaourt -Rs $(yaourt -Qdtq))...'\033[0m\n"
 yaourt -Rs $(yaourt -Qdtq) --noconfirm
 
-echo -e "\n\033[1;32mLet's do cephalus-manjaro backup (to thrasymachus-manjaro)...\033[0m\n"
-/home/mpurcell/bin/cephalus_backup
+echo -e "\n\033[1;32mLet's do thrasymachus-manjaro backup (to ovid)...\033[0m\n"
+/home/mpurcell/bin/thrasymachus_backup
 
