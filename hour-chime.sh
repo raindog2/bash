bc68c2d1499c03f81790b859ea1b2d9d292b2d51
cephalus <none>
2016-09-02 01:03:28 -0700

    better hour chime

--- hour-chime.sh
+++ hour-chime.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 time=`date +%l:%M%P`
+hour=`date +%l`
 /usr/bin/notify-send -t 5000 "The hour has struck!"
 /usr/bin/notify-send -t 5000 "It is $time."
-/usr/bin/paplay /home/mpurcell/music/chime_big_ben.wav
+#paplay /usr/share/sounds/speech-dispatcher/test.wav
+for n in $(seq $hour);
+    do paplay ~/music/tone.wav;
+done
